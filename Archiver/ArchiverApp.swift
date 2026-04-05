dce25a9c171743ce3e8703ec4e4d23554e9c9dce
alishapckg <alishaproton@proton.me>
2026-04-05 20:24:09 +0300

    Initial Commit

---
+++ Archiver/ArchiverApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArchiverApp.swift
+//  Archiver
+//
+//  Created by NS on 4/5/26.
+//
+
+import SwiftUI
+
+@main
+struct ArchiverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
